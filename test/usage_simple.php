7609e568c79985d779f91b9d626dc35db823046d
Minh Le <lqm1010@gmail.com>
2013-09-04 09:09:22 +0700

    add 3 new api: two_way_sms, soft_token, push

--- test/usage_simple.php
+++ test/usage_simple.php
@@ -15,7 +15,10 @@ $secret_key = TEST_SECRET_KEY;
 
 $verify = new Verify($customer_id, $secret_key);
 
-$phone_number = "13103409700";
+$phone_number = "13105551234";
 $result = $verify->call($phone_number);
+// $result = $verify->two_way_sms($phone_number, "TRVF", "123456", "1m");
+// $result = $verify->sms($phone_number, "123123");
+// $result = $verify->soft_token($phone_number, "", "123123");
 
 print_r($result);
