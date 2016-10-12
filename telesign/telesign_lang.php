a94fb463449ef6bf87e83fc267c2f31fdd58845b
PHPLicengine <phplicengine@gmail.com>
2016-10-12 17:35:38 +0330

    TeleSign Language Selector
    We had created this drop down menu that end-users can choose their desired language on form where they enter their phone number too. As we wasted our time to create this form and were forced to remove TeleSign integration from our product, we contribute it here that whenever TeleSign got a user in future, they can use it saving their time to create the form again.

---
+++ telesign/telesign_lang.php
@@ -0,0 +1,100 @@
+<select id="D_TeleSign_lang" name="TeleSign_lang">
+  <option value="en" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "en") { echo 'selected="selected"'; } ?> >English</option>
+  <option value="fr" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "fr") { echo 'selected="selected"'; } ?> >French</option>
+  <option value="de" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "de") { echo 'selected="selected"'; } ?> >German</option>
+  <option value="es" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "es") { echo 'selected="selected"'; } ?> >Spanish</option>
+  <option value="af" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "af") { echo 'selected="selected"'; } ?> >Afrikaans</option>
+  <option value="sq" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sq") { echo 'selected="selected"'; } ?> >Albanian</option>
+  <option value="ar" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ar") { echo 'selected="selected"'; } ?> >Arabic</option>
+  <option value="ar-EG" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ar-EG") { echo 'selected="selected"'; } ?> >Arabic (Egypt)</option>
+  <option value="hy" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "hy") { echo 'selected="selected"'; } ?> >Armenian</option>
+  <option value="bn" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "bn") { echo 'selected="selected"'; } ?> >Bangla</option>
+  <option value="eu" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "eu") { echo 'selected="selected"'; } ?> >Basque</option>
+  <option value="be" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "be") { echo 'selected="selected"'; } ?> >Belarusian</option>
+  <option value="bn-BD" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "bn-BD") { echo 'selected="selected"'; } ?> >Bengali</option>
+  <option value="bs" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "bs") { echo 'selected="selected"'; } ?> >Bosnian</option>
+  <option value="bg" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "bg") { echo 'selected="selected"'; } ?> >Bengali</option>
+  <option value="bn-BD" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "bn-BD") { echo 'selected="selected"'; } ?> >Bulgarian</option>
+  <option value="ca" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ca") { echo 'selected="selected"'; } ?> >Catalan</option>
+  <option value="ku" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ku") { echo 'selected="selected"'; } ?> >Central Kurdish</option>
+  <option value="zh-HK" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "zh-HK") { echo 'selected="selected"'; } ?> >Chinese (Hong Kong, Cantonese)</option>
+  <option value="zh-CN" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "zh-CN") { echo 'selected="selected"'; } ?> >Chinese (Mandarin)</option>
+  <option value="zh-TW" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "zh-TW") { echo 'selected="selected"'; } ?> >Chinese (Taiwan)</option>
+  <option value="hr" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "hr") { echo 'selected="selected"'; } ?> >Croatian</option>
+  <option value="cs" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "cs") { echo 'selected="selected"'; } ?> >Czech</option>
+  <option value="da" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "da") { echo 'selected="selected"'; } ?> >Danish</option>
+  <option value="fa-AF" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "fa-AF") { echo 'selected="selected"'; } ?> >Dari</option>
+  <option value="nl" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "nl") { echo 'selected="selected"'; } ?> >Dutch</option>
+  <option value="egx" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "egz") { echo 'selected="selected"'; } ?> >Egyptian</option>
+  <option value="en-AU" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "en-AU") { echo 'selected="selected"'; } ?> >English (Australian</option>
+  <option value="en-GB" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "en-GB") { echo 'selected="selected"'; } ?> >English (UK)</option>
+  <option value="en-US" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "en-US") { echo 'selected="selected"'; } ?> >English (US)</option>
+  <option value="en-CA" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "en-CA") { echo 'selected="selected"'; } ?> >English (Canadian</option>
+  <option value="en-IN" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "en-IN") { echo 'selected="selected"'; } ?> >English (India)</option>
+  <option value="et" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "et") { echo 'selected="selected"'; } ?> >Estonian</option>
+  <option value="fil" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "fil") { echo 'selected="selected"'; } ?> >Filipino</option>
+  <option value="fi" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "fi") { echo 'selected="selected"'; } ?> >Finnish</option>
+  <option value="fr-CA" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "fr-CA") { echo 'selected="selected"'; } ?> >French (Canadian)</option>
+  <option value="gl" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "gl") { echo 'selected="selected"'; } ?> >Galician</option>
+  <option value="ka" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ka") { echo 'selected="selected"'; } ?> >Georgian</option>
+  <option value="el" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "el") { echo 'selected="selected"'; } ?> >Greek</option>
+  <option value="gu" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "gu") { echo 'selected="selected"'; } ?> >Gujarati</option>
+  <option value="ha" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ha") { echo 'selected="selected"'; } ?> >Hausa</option>
+  <option value="he" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "he") { echo 'selected="selected"'; } ?> >Hebrew</option>
+  <option value="hi" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "hi") { echo 'selected="selected"'; } ?> >Hindi</option>
+  <option value="hu" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "hu") { echo 'selected="selected"'; } ?> >Hungarian</option>
+  <option value="is" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "is") { echo 'selected="selected"'; } ?> >Icelandic</option>
+  <option value="id" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "id") { echo 'selected="selected"'; } ?> >Indonesian</option>
+  <option value="zu" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "zu") { echo 'selected="selected"'; } ?> >isiZulu</option>
+  <option value="it" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "it") { echo 'selected="selected"'; } ?> >Italian</option>
+  <option value="ja" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ja") { echo 'selected="selected"'; } ?> >Japanese</option>
+  <option value="kn" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "kn") { echo 'selected="selected"'; } ?> >Kannada</option>
+  <option value="kk" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "kk") { echo 'selected="selected"'; } ?> >Kazakh</option>
+  <option value="km" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "km") { echo 'selected="selected"'; } ?> >Khmer</option>
+  <option value="sw" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sw") { echo 'selected="selected"'; } ?> >Kiswahili</option>
+  <option value="kok" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "kok") { echo 'selected="selected"'; } ?> >Konkani</option>
+  <option value="ko" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ko") { echo 'selected="selected"'; } ?> >Korean</option>
+  <option value="ky" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ky") { echo 'selected="selected"'; } ?> >Kyrgyz</option>
+  <option value="lv" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "lv") { echo 'selected="selected"'; } ?> >Latvian</option>
+  <option value="ln" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ln") { echo 'selected="selected"'; } ?> >Lingala</option>
+  <option value="lt" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "lt") { echo 'selected="selected"'; } ?> >Lithuanian</option>
+  <option value="lb" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "lb") { echo 'selected="selected"'; } ?> >Luxembourgish</option>
+  <option value="mk" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "mk") { echo 'selected="selected"'; } ?> >Macedonian</option>
+  <option value="ms" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ms") { echo 'selected="selected"'; } ?> >Malay</option>
+  <option value="ml" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ml") { echo 'selected="selected"'; } ?> >Malayalam</option>
+  <option value="mr" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "mr") { echo 'selected="selected"'; } ?> >Marathi</option>
+  <option value="ne" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ne") { echo 'selected="selected"'; } ?> >Nepali</option>
+  <option value="no" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "no") { echo 'selected="selected"'; } ?> >Norweigan</option>
+  <option value="or" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "or") { echo 'selected="selected"'; } ?> >Odia</option>
+  <option value="fa" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "fa") { echo 'selected="selected"'; } ?> >Persian (Farsi)</option>
+  <option value="pl" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "pl") { echo 'selected="selected"'; } ?> >Polish</option>
+  <option value="pt" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "pt") { echo 'selected="selected"'; } ?> >Portuguese</option>
+  <option value="pt-BR" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "pt-BR") { echo 'selected="selected"'; } ?> >Portuguese (Brazil)</option>
+  <option value="pt-PT" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "pt-PT") { echo 'selected="selected"'; } ?> >Portuguese (Europe)</option>
+  <option value="pa-IN" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "pa-IN") { echo 'selected="selected"'; } ?> >Punjabi</option>
+  <option value="pa-PK" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "pa-PK") { echo 'selected="selected"'; } ?> >Punjabi (Pakistan)</option>
+  <option value="ro" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ro") { echo 'selected="selected"'; } ?> >Romanian</option>
+  <option value="ru" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ru") { echo 'selected="selected"'; } ?> >Russian</option>
+  <option value="sr" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sr") { echo 'selected="selected"'; } ?> >Serbian</option>
+  <option value="sr-BA" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sr-BA") { echo 'selected="selected"'; } ?> >Serbian (Latin)</option>
+  <option value="sd" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sd") { echo 'selected="selected"'; } ?> >Sindhi</option>
+  <option value="sk" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sk") { echo 'selected="selected"'; } ?> >Slovak</option>
+  <option value="sk-SK" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sk-SK") { echo 'selected="selected"'; } ?> >Slovakian</option>
+  <option value="sl" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sl") { echo 'selected="selected"'; } ?> >Slovenian</option>
+  <option value="es-ES" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "es-ES") { echo 'selected="selected"'; } ?> >Spanish (Europe)</option>
+  <option value="es-CL" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "es-CL") { echo 'selected="selected"'; } ?> >Spanish (Chile)</option>
+  <option value="es-419" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "es-419") { echo 'selected="selected"'; } ?> >Spanish (Latin America)</option>
+  <option value="es-MX" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "es-MX") { echo 'selected="selected"'; } ?> >Spanish (Mexico)</option>
+  <option value="es-ES" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "es-ES") { echo 'selected="selected"'; } ?> >Spanish (Spain, Castilian)</option>
+  <option value="ca" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ca") { echo 'selected="selected"'; } ?> >Spanish (Catalan)</option>
+  <option value="sv" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "sv") { echo 'selected="selected"'; } ?> >Swedish</option>
+  <option value="gsw" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "gsw") { echo 'selected="selected"'; } ?> >Swiss German</option>
+  <option value="ta" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ta") { echo 'selected="selected"'; } ?> >Tamil</option>
+  <option value="te" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "te") { echo 'selected="selected"'; } ?> >Telugu</option>
+  <option value="th" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "th") { echo 'selected="selected"'; } ?> >Thai</option>
+  <option value="tr" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "tr") { echo 'selected="selected"'; } ?> >Turkish</option>
+  <option value="uk" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "uk") { echo 'selected="selected"'; } ?> >Ukrainian</option>
+  <option value="ur" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "ur") { echo 'selected="selected"'; } ?> >Urdu</option>
+  <option value="vi" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "vi") { echo 'selected="selected"'; } ?> >Vietnamese</option>
+  <option value="cy" <?php if (isset($_POST['TeleSign_lang']) &&  $_POST['TeleSign_lang'] == "cy") { echo 'selected="selected"'; } ?> >Welsh</option>
+</select>
