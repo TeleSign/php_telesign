232cb47bc326338daa0d784ede15e6e64fdc6b9a
Mousumi Mohanty <mmohanty@telesign.com>
2026-01-15 15:45:33 +0000

    Pull request #25: Feature/EOA-5903 SS Intellegence
    Merge in SDK/php_telesign from feature/EOA-5903-SS-Intellegence to developer
    
    Squashed commit of the following:
    
    commit 4b2de55a789441090168e0a23f4992a70ce255c5
    Merge: 7a95ab1 b7c3190
    Author: Juan Camilo Martinez <jmartinez@telesign.com>
    Date:   Thu Jan 15 10:43:39 2026 -0500
    
        Solve issues merge
    
    commit 7a95ab10cb538f530060d80207f5730dd0c4523e
    Author: MousumiMohanty <@telesign.com>
    Date:   Thu Oct 23 16:09:37 2025 +0530
    
        update the release file
    
    commit 94efe5beb37d9d7eca4fc91bd118362891c74cf6
    Author: MousumiMohanty <@telesign.com>
    Date:   Thu Oct 23 13:36:03 2025 +0530
    
        Added support for Intelligence Cloud to use new endpoint
    
    commit 48f47b8bd6499d955efd7fab528c1389ea52d505
    Author: Mousumi Mohanty <mmohanty@telesign.com>
    Date:   Fri Oct 24 08:45:14 2025 +0000
    
        Pull request #24: feature/EOA-5599-Update-psr-http-message
    
        Merge in SDK/php_telesign from feature/EOA-5599-Update-psr-http-message to developer
    
        Squashed commit of the following:
    
        commit 4be027844b6387198dfd547374363f32da1e4687
        Author: MousumiMohanty <@telesign.com>
        Date:   Thu Oct 23 15:46:05 2025 +0530
    
            Updated psr/http-message dependency to allow both v1 and v2

--- test/score/ScoreClientTest.php
+++ test/score/ScoreClientTest.php
@@ -5,28 +5,30 @@ namespace telesign\sdk\score;
 use telesign\sdk\Example;
 use telesign\sdk\ClientTest;
 
-final class ScoreClientTest extends ClientTest {
-
+final class ScoreClientTest extends ClientTest
+{
   const EXAMPLE_PHONE_NUMBER = Example::PHONE_NUMBER;
   const EXAMPLE_ACCOUNT_LIFECYCLE_EVENT = Example::ACCOUNT_LIFECYCLE_EVENT;
 
-  function getRequestExamples () {
-    return [
+  function getRequestExamples()
+  {
+    return 
+    [
       [
         ScoreClient::class,
         "score",
-        [
-          self::EXAMPLE_PHONE_NUMBER,
-          self::EXAMPLE_ACCOUNT_LIFECYCLE_EVENT,
-          [ "optional_param" => "123" ]
-        ],
-        self::EXAMPLE_REST_ENDPOINT . "/v1/score/" . self::EXAMPLE_PHONE_NUMBER,
-        [
-          "account_lifecycle_event" => self::EXAMPLE_ACCOUNT_LIFECYCLE_EVENT,
-          "optional_param" => "123"
+          [
+            self::EXAMPLE_PHONE_NUMBER,
+            self::EXAMPLE_ACCOUNT_LIFECYCLE_EVENT,
+            [ "optional_param" => "123" ]
+          ],
+          self::EXAMPLE_REST_ENDPOINT . "/intelligence/phone", 
+          [
+            "phone_number" => self::EXAMPLE_PHONE_NUMBER,      
+            "account_lifecycle_event" => self::EXAMPLE_ACCOUNT_LIFECYCLE_EVENT,
+            "optional_param" => "123"
+          ]
         ]
-      ]
-    ];
-  }
-
+      ];
+    }
 }
